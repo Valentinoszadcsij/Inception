37967647bc019c762cc34d77572f0aa955801e30
Valentin Oszadcsij <valentinosadchiy@gmail.com>
2024-06-27 17:19:46 +0200

    update script and dockerfile, add .env, update cheatsheet

---
+++ srcs/mariadb/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# initialize MariaDB data directory
+if [ ! -d /var/lib/mysql/mysql ]; then
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
+# start MariaDB service 
+mysqld_safe --datadir=/var/lib/mysql
+
+# create database and user
+mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
+mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
