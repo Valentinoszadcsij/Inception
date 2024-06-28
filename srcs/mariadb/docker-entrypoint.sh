8098e5d88a9be0900d2f42aa3dccea0cd04b99fe
Valentin Oszadcsij <valentinosadchiy@gmail.com>
2024-06-28 16:57:27 +0200

    not much changes

--- srcs/mariadb/docker-entrypoint.sh
+++ srcs/mariadb/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 # initialize MariaDB data directory
-if [ ! -d /var/lib/mysql/mysql ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
+# if [ ! -d /var/lib/mysql/mysql ]; then
+# 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# fi
 
 # start MariaDB service 
 mysqld_safe --datadir=/var/lib/mysql
-
+echo "hello world" > hello-world.txt
 # create database and user
 mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
