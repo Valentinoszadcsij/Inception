18bd82d30216a86e507a7d0d7c972ed15ff1b54b
voszadcs <valentinosadchiy@gmail.com>
2024-07-20 10:48:36 -0700

    almost done: nginx doesnt connect to wp

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -10,8 +10,13 @@ wp config create --allow-root --path="/var/www/html" --dbname="$MARIADB_DATABASE
 wp core install --allow-root --path="/var/www/html" --url=https://voszadcs.42.fr \
 	--admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" \
 	--admin_email="voszadcs@student.42heilbronn.de" --title="inception" --skip-email
-wp user create --allow-root $WP_USER_NAME "valentinosadchiy@gmail.com" \
-    --role=author --user_pass="$WP_USER_PASSWORD"
+if ! wp user get "$WP_USER_NAME" --field=ID --allow-root > /dev/null 2>&1; then
+    # If the user does not exist, create it
+    wp user create --allow-root "$WP_USER_NAME" "valentinosadchiy@gmail.com" \
+        --role=author --user_pass="$WP_USER_PASSWORD"
+else
+    echo "User $WP_USER_NAME already exists. Skipping creation."
+fi
 
 # Set up WordPress directory permissions
 chown -R www-data:www-data /var/www/html
